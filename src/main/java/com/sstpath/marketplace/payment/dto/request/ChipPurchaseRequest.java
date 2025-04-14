a5633d18ebb061010823727006c30ede0371705e
'durga-sstpath' <'perapu.durga.prasad@sstpath.com'>
2025-04-14 14:04:19 +0530

    Mohan - Payment Service

--- src/main/java/com/sstpath/marketplace/payment/dto/request/ChipPurchaseRequest.java
+++ src/main/java/com/sstpath/marketplace/payment/dto/request/ChipPurchaseRequest.java
@@ -8,18 +8,34 @@ import java.util.List;
 public class ChipPurchaseRequest {
     private String brand_id;
     private String external_id;
-    private String name;
-    private String email;
-    private Integer amount;
-    private String currency;
+    private ClientInfo client;
+    private PurchaseDetails purchase;
     private String success_redirect;
+    private String failure_redirect;
     private String success_callback;
-    private List<Product> products;
+    private String orderRef;
+    
+    @Data
+    public static class ClientInfo {
+        private String email;
+        private String phone;
+        private String name;
+    }
 
     @Data
-    public static class Product {
-        private String name;
-        private Integer price;
-        private Integer quantity;
+    public static class PurchaseDetails {
+        private List<Product> products;
+        private int total;
+        private String currency;
+        private String invoice;
+
+        @Data
+        public static class Product {
+            private String name;
+            private int quantity;
+            private int price;
+        }
     }
 }
+
+
