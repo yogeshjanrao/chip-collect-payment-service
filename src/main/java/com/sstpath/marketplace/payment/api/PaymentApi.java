c5d4a0dba7343864bd660ff88a463c462dee177f
yogesh-sstpath <yogesh.s.janrao@sstpath.com>
2025-04-15 21:15:20 +0530

    change in properties

--- src/main/java/com/sstpath/marketplace/payment/api/PaymentApi.java
+++ src/main/java/com/sstpath/marketplace/payment/api/PaymentApi.java
@@ -2,7 +2,6 @@ package com.sstpath.marketplace.payment.api;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 public interface PaymentApi {
 
